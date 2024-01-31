bc77c380a313f0c2f15855aedb17173139e6ed64
MarcOreliOoo <albeaurelien@gmail.com>
2024-01-31 19:02:37 +0100

    fix: escape &apos

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,10 +6,10 @@ const AboutPage = () => {
 			<h1>About Me</h1>
 			<h2>Diving Deeper into My Journey</h2>
 			<p>
-				After introducing myself in the hero section, I'd love to share
-				more about my journey in the dynamic world of technology. My
-				passion for Web3 started in early 2020 in the classic way with
-				crypto and centralized exchanges, only to leave them and
+				After introducing myself in the hero section, I&apos;d love to
+				share more about my journey in the dynamic world of technology.
+				My passion for Web3 started in early 2020 in the classic way
+				with crypto and centralized exchanges, only to leave them and
 				discover DEX. Little by little, I lifted the hood to understand
 				how smart contracts, blockchain in general and Defi worked... I
 				discovered a fascinating world and from the hole I fell into, I
@@ -17,7 +17,7 @@ const AboutPage = () => {
 			</p>
 			<h2>Experience and Achievements</h2>
 			<p>
-				Over the years, I've had the opportunity to work on several
+				Over the years, I&apos;ve had the opportunity to work on several
 				personal and professional projects, and hackathons. Each project
 				has been a driving force for me to improve my skills in smart
 				contracts, API integration, frontend and backend development,
@@ -30,10 +30,10 @@ const AboutPage = () => {
 			<p>
 				The landscape of data and tech in general is constantly
 				changing. The Web3 landscape is even more so, requiring a
-				commitment to continuous adaptation and development. It's an
-				absorbing but enjoyable challenge! Collaboration is at the heart
-				of my approach - I enjoy working with teams, sharing knowledge
-				and learning from others.
+				commitment to continuous adaptation and development. It&apos;s
+				an absorbing but enjoyable challenge! Collaboration is at the
+				heart of my approach - I enjoy working with teams, sharing
+				knowledge and learning from others.
 			</p>
 			<h2>Fusing Web3 Innovation with Data Engineering</h2>
 			<p>
@@ -41,16 +41,16 @@ const AboutPage = () => {
 				data engineering practices. This blend allows me to not only
 				develop decentralized applications but also to make these
 				applications smarter and more user-friendly with data-driven
-				insights. I'm excited about the practical, real-world
+				insights. I&apos;m excited about the practical, real-world
 				applications of this fusion, where technology meets usability.
 			</p>
 			<h2>Looking Forward</h2>
 			<p>
 				As I look forward to my next adventure, my goal is to join a
 				team or project where I can contribute significantly, especially
-				in the realm of Web3 and full-stack development. I'm excited
-				about the opportunities to innovate, collaborate, and develop
-				solutions that make a difference.
+				in the realm of Web3 and full-stack development. I&apos;m
+				excited about the opportunities to innovate, collaborate, and
+				develop solutions that make a difference.
 			</p>
 		</div>
 	);
